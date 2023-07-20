30c554d8d4a5cb2213af674ba2d5c6c9e1b8ce8e
DESKTOP-JJQ176Q\Issey Sone <issey.d.sone@gmail.com>
2023-07-20 18:40:32 -0400

    Issey - Added Register page, sign up button changes to Register Activity

--- app/src/main/java/com/example/b07_project_group5/LoginActivity.java
+++ app/src/main/java/com/example/b07_project_group5/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.b07_project_group5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -59,7 +60,8 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    public void navigateToSignUpScreen(View v) {
-
+    public void navigateToSignUpActivity(View v) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/b07_project_group5/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.b07_project_group5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Intent intent = getIntent();
+    }
+}
