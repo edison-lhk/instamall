c2e8deee3eabbeb92f45eb4ee6d36b4880c50952
Edison Liem <edisonliem417@gmail.com>
2023-07-20 23:08:15 -0400

    Edison - Implement the dropdown list for user type in the login UI

--- app/src/main/java/com/example/b07_project_group5/LoginActivity.java
+++ app/src/main/java/com/example/b07_project_group5/LoginActivity.java
@@ -3,7 +3,11 @@ package com.example.b07_project_group5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,12 +20,24 @@ import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
     FirebaseDatabase db;
+    String type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         db = FirebaseDatabase.getInstance("https://testing-7a8a5-default-rtdb.firebaseio.com/");
+        type = "";
+        String[] user_types = getResources().getStringArray(R.array.user_types);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.dropdown_item, user_types);
+        AutoCompleteTextView login_type_input = (AutoCompleteTextView) findViewById(R.id.login_type_input);
+        login_type_input.setAdapter(arrayAdapter);
+        login_type_input.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                type = parent.getItemAtPosition(position).toString().toLowerCase();
+            }
+        });
     }
 
     public void setWarningText(String warning) {
@@ -34,20 +50,30 @@ public class LoginActivity extends AppCompatActivity {
         String email = email_input.getText().toString();
         EditText password_input = (EditText) findViewById(R.id.login_password_input);
         String password = password_input.getText().toString();
-        if (email.equals("") || password.equals("")) {
+        if (email.equals("") || password.equals("") || type.equals("")) {
             setWarningText(getString(R.string.login_empty_fields_warning));
             return;
         }
         DatabaseReference ref = db.getReference();
-        DatabaseReference query = ref.child("users").child(email);
-        query.addValueEventListener(new ValueEventListener() {
+        DatabaseReference query = ref.child("users");
+        query.orderByChild("email").equalTo(email).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
                 if (!snapshot.exists()) {
                     setWarningText(getString(R.string.login_cannot_find_user_warning));
                 } else {
-                    if (!password.equals(snapshot.child("password").getValue().toString())) {
-                        setWarningText(getString(R.string.login_password_incorrect_warning));
+                    boolean user_exist = false;
+                    for (DataSnapshot childSnapshot: snapshot.getChildren()) {
+                        if (type.equals(childSnapshot.child("type").getValue().toString())) {
+                            if (!password.equals(childSnapshot.child("password").getValue().toString())) {
+                                setWarningText(getString(R.string.login_password_incorrect_warning));
+                                return;
+                            }
+                            user_exist = true;
+                        }
+                    }
+                    if (!user_exist) {
+                        setWarningText(getString(R.string.login_cannot_find_user_warning));
                         return;
                     }
                     Toast.makeText(LoginActivity.this, getString(R.string.login_successful_text), Toast.LENGTH_LONG).show();
