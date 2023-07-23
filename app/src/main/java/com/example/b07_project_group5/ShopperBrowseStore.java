b33fe6310270c4b00ac1936141a503cf6f1c08a3
Hans0524 <you@example.com>
2023-07-23 00:35:56 -0400

    Shopper Browse Store page UI Design & Method for automatically generated Button base on database

---
+++ app/src/main/java/com/example/b07_project_group5/ShopperBrowseStore.java
@@ -0,0 +1,27 @@
+package com.example.b07_project_group5;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import android.os.Bundle;
+
+public class ShopperBrowseStore extends AppCompatActivity {
+    FirebaseDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        db = FirebaseDatabase.getInstance("https://testing-7a8a5-default-rtdb.firebaseio.com/");
+
+    }
+}
