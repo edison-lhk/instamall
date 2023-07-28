dac4b61e1d113128f4f52159ee47459b594b79e1
Edison Liem <edisonliem417@gmail.com>
2023-07-28 02:12:51 -0400

    Edison - Remove unnecessary imports from login activity

--- app/src/main/java/com/example/b07_project_group5/LoginActivity.java
+++ app/src/main/java/com/example/b07_project_group5/LoginActivity.java
@@ -3,7 +3,6 @@ package com.example.b07_project_group5;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
