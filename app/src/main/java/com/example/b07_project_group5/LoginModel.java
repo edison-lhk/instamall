b8554321d09954ba6a983b7f071482434cacc0c4
Edison Liem <edisonliem417@gmail.com>
2023-08-03 22:11:16 -0400

    Edison - Fix login bugs

--- app/src/main/java/com/example/b07_project_group5/LoginModel.java
+++ app/src/main/java/com/example/b07_project_group5/LoginModel.java
@@ -46,6 +46,7 @@ public class LoginModel implements LoginContract.Model {
                 for (DataSnapshot userSnapshot: snapshot.getChildren()) {
                     if (accountType.equals(userSnapshot.child("accountType").getValue().toString())) {
                         callback.onSuccess(userSnapshot.getKey());
+                        return;
                     }
                 }
                 callback.onFailure();
@@ -98,7 +99,8 @@ public class LoginModel implements LoginContract.Model {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if (!snapshot.exists()) {
-                    Store store = new Store(userId, username.substring(0, 4) + "'s Store", "https://firebasestorage.googleapis.com/v0/b/testing-7a8a5.appspot.com/o/StoreLogo%2Fstore_logo.png?alt=media&token=2660325b-c4cd-4659-bf6b-60239d06f84b");
+                    String storeName = username.length() >= 4 ? username.substring(0, 4) + "'s Store" : username + "'s Store";
+                    Store store = new Store(userId, storeName, "https://firebasestorage.googleapis.com/v0/b/testing-7a8a5.appspot.com/o/StoreLogo%2Fstore_logo.png?alt=media&token=2660325b-c4cd-4659-bf6b-60239d06f84b");
                     String uniqueKey = ref.push().getKey();
                     query.child(uniqueKey).setValue(store).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
