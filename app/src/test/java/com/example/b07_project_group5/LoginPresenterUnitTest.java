a8b8c28fdce4d4e3b40558f2dbea1b133cc3e873
Edison Liem <edisonliem417@gmail.com>
2023-08-06 14:41:25 -0400

    Edison - Finish implementing all working JUnit tests and achieve 100% line coverage of the login presenter

--- app/src/test/java/com/example/b07_project_group5/LoginPresenterUnitTest.java
+++ app/src/test/java/com/example/b07_project_group5/LoginPresenterUnitTest.java
@@ -3,8 +3,14 @@ package com.example.b07_project_group5;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -61,19 +67,17 @@ public class LoginPresenterUnitTest {
         when(view.getEmailInput()).thenReturn("usernotfound@gmail.com");
         when(view.getPasswordInput()).thenReturn("12345");
         when(view.getAccountTypeInput()).thenReturn("shopper");
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                LoginContract.Model.findUserWithEmailCallback findUserWithEmailCallback = invocation.getArgument(1);
+                findUserWithEmailCallback.onFailure();
+                return null;
+            }
+        }).when(model).findUserWithEmail(eq("usernotfound@gmail.com"), any(LoginContract.Model.findUserWithEmailCallback.class));
         LoginPresenter presenter = new LoginPresenter(view, model);
         presenter.loginUser();
         verify(view).setWarningText(view.getStringFromResource(R.string.login_cannot_find_user_warning));
-    }
-
-    @Test
-    public void testIncorrectPassword() {
-        when(view.getEmailInput()).thenReturn("edison@gmail.com");
-        when(view.getPasswordInput()).thenReturn("12345");
-        when(view.getAccountTypeInput()).thenReturn("owner");
-        LoginPresenter presenter = new LoginPresenter(view, model);
-        presenter.loginUser();
-        verify(view).setWarningText(view.getStringFromResource(R.string.login_password_incorrect_warning));
     }
 
     @Test
@@ -81,21 +85,59 @@ public class LoginPresenterUnitTest {
         when(view.getEmailInput()).thenReturn("vincent@gmail.com");
         when(view.getPasswordInput()).thenReturn("vincent");
         when(view.getAccountTypeInput()).thenReturn("shopper");
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                LoginContract.Model.findUserWithEmailCallback findUserWithEmailCallback = invocation.getArgument(1);
+                findUserWithEmailCallback.onSuccess();
+                return null;
+            }
+        }).when(model).findUserWithEmail(eq("vincent@gmail.com"), any(LoginContract.Model.findUserWithEmailCallback.class));
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                LoginContract.Model.getUserIdByEmailAndAccountTypeCallback getUserIdByEmailAndAccountTypeCallback = invocation.getArgument(2);
+                getUserIdByEmailAndAccountTypeCallback.onFailure();
+                return null;
+            }
+        }).when(model).getUserIdByEmailAndAccountType(eq("vincent@gmail.com"), eq("shopper"), any(LoginContract.Model.getUserIdByEmailAndAccountTypeCallback.class));
         LoginPresenter presenter = new LoginPresenter(view, model);
         presenter.loginUser();
         verify(view).setWarningText(view.getStringFromResource(R.string.login_account_type_incorrect_warning));
     }
 
     @Test
-    public void testValidStoreOwner() {
+    public void testIncorrectPassword() {
         when(view.getEmailInput()).thenReturn("edison@gmail.com");
-        when(view.getPasswordInput()).thenReturn("edison");
+        when(view.getPasswordInput()).thenReturn("12345");
         when(view.getAccountTypeInput()).thenReturn("owner");
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                LoginContract.Model.findUserWithEmailCallback findUserWithEmailCallback = invocation.getArgument(1);
+                findUserWithEmailCallback.onSuccess();
+                return null;
+            }
+        }).when(model).findUserWithEmail(eq("edison@gmail.com"), any(LoginContract.Model.findUserWithEmailCallback.class));
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                LoginContract.Model.getUserIdByEmailAndAccountTypeCallback getUserIdByEmailAndAccountTypeCallback = invocation.getArgument(2);
+                getUserIdByEmailAndAccountTypeCallback.onSuccess("-NaKjb3W2PIULK3_yjjW");
+                return null;
+            }
+        }).when(model).getUserIdByEmailAndAccountType(eq("edison@gmail.com"), eq("owner"), any(LoginContract.Model.getUserIdByEmailAndAccountTypeCallback.class));
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                LoginContract.Model.checkUserPasswordIsCorrectCallback checkUserPasswordIsCorrectCallback = invocation.getArgument(2);
+                checkUserPasswordIsCorrectCallback.onFailure();
+                return null;
+            }
+        }).when(model).checkUserPasswordIsCorrect(eq("-NaKjb3W2PIULK3_yjjW"), eq("12345"), any(LoginContract.Model.checkUserPasswordIsCorrectCallback.class));
         LoginPresenter presenter = new LoginPresenter(view, model);
         presenter.loginUser();
-        verify(view).clearInputFields();
-        verify(view).setWarningText("");
-        verify(view).showToastMessage();
+        verify(view).setWarningText(view.getStringFromResource(R.string.login_password_incorrect_warning));
     }
 
     @Test
@@ -103,10 +145,96 @@ public class LoginPresenterUnitTest {
         when(view.getEmailInput()).thenReturn("edison@gmail.com");
         when(view.getPasswordInput()).thenReturn("edison");
         when(view.getAccountTypeInput()).thenReturn("shopper");
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                LoginContract.Model.findUserWithEmailCallback findUserWithEmailCallback = invocation.getArgument(1);
+                findUserWithEmailCallback.onSuccess();
+                return null;
+            }
+        }).when(model).findUserWithEmail(eq("edison@gmail.com"), any(LoginContract.Model.findUserWithEmailCallback.class));
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                LoginContract.Model.getUserIdByEmailAndAccountTypeCallback getUserIdByEmailAndAccountTypeCallback = invocation.getArgument(2);
+                getUserIdByEmailAndAccountTypeCallback.onSuccess("-NaKnQ_i7VUDQVW__YQ2");
+                return null;
+            }
+        }).when(model).getUserIdByEmailAndAccountType(eq("edison@gmail.com"), eq("shopper"), any(LoginContract.Model.getUserIdByEmailAndAccountTypeCallback.class));
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                LoginContract.Model.checkUserPasswordIsCorrectCallback checkUserPasswordIsCorrectCallback = invocation.getArgument(2);
+                checkUserPasswordIsCorrectCallback.onSuccess();
+                return null;
+            }
+        }).when(model).checkUserPasswordIsCorrect(eq("-NaKnQ_i7VUDQVW__YQ2"), eq("edison"), any(LoginContract.Model.checkUserPasswordIsCorrectCallback.class));
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                LoginContract.Model.getUsernameByIdCallback getUsernameByIdCallback = invocation.getArgument(1);
+                getUsernameByIdCallback.onSuccess("edison");
+                return null;
+            }
+        }).when(model).getUsernameById(eq("-NaKnQ_i7VUDQVW__YQ2"), any(LoginContract.Model.getUsernameByIdCallback.class));
         LoginPresenter presenter = new LoginPresenter(view, model);
         presenter.loginUser();
         verify(view).clearInputFields();
         verify(view).setWarningText("");
         verify(view).showToastMessage();
+        verify(view).navigateToBrowseStoreActivity("-NaKnQ_i7VUDQVW__YQ2");
+    }
+
+    @Test
+    public void testValidStoreOwner() {
+        when(view.getEmailInput()).thenReturn("edison@gmail.com");
+        when(view.getPasswordInput()).thenReturn("edison");
+        when(view.getAccountTypeInput()).thenReturn("owner");
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                LoginContract.Model.findUserWithEmailCallback findUserWithEmailCallback = invocation.getArgument(1);
+                findUserWithEmailCallback.onSuccess();
+                return null;
+            }
+        }).when(model).findUserWithEmail(eq("edison@gmail.com"), any(LoginContract.Model.findUserWithEmailCallback.class));
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                LoginContract.Model.getUserIdByEmailAndAccountTypeCallback getUserIdByEmailAndAccountTypeCallback = invocation.getArgument(2);
+                getUserIdByEmailAndAccountTypeCallback.onSuccess("-NaKjb3W2PIULK3_yjjW");
+                return null;
+            }
+        }).when(model).getUserIdByEmailAndAccountType(eq("edison@gmail.com"), eq("owner"), any(LoginContract.Model.getUserIdByEmailAndAccountTypeCallback.class));
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                LoginContract.Model.checkUserPasswordIsCorrectCallback checkUserPasswordIsCorrectCallback = invocation.getArgument(2);
+                checkUserPasswordIsCorrectCallback.onSuccess();
+                return null;
+            }
+        }).when(model).checkUserPasswordIsCorrect(eq("-NaKjb3W2PIULK3_yjjW"), eq("edison"), any(LoginContract.Model.checkUserPasswordIsCorrectCallback.class));
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                LoginContract.Model.getUsernameByIdCallback getUsernameByIdCallback = invocation.getArgument(1);
+                getUsernameByIdCallback.onSuccess("edisonlol");
+                return null;
+            }
+        }).when(model).getUsernameById(eq("-NaKjb3W2PIULK3_yjjW"), any(LoginContract.Model.getUsernameByIdCallback.class));
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                LoginContract.Model.createStoreForOwnerCallback createStoreForOwnerCallback = invocation.getArgument(2);
+                createStoreForOwnerCallback.onSuccess("-NaKjfc7qHvl0pgOt-uB");
+                return null;
+            }
+        }).when(model).createStoreForOwner(eq("-NaKjb3W2PIULK3_yjjW"), eq("edisonlol"), any(LoginContract.Model.createStoreForOwnerCallback.class));
+        LoginPresenter presenter = new LoginPresenter(view, model);
+        presenter.loginUser();
+        verify(view).clearInputFields();
+        verify(view).setWarningText("");
+        verify(view).showToastMessage();
+        verify(view).navigateToStoreActivity("-NaKjb3W2PIULK3_yjjW", "-NaKjfc7qHvl0pgOt-uB");
     }
 }
